1c72cc17a9442476a894c9fc6f2ff57befd91260
tangxianyun <xianyun.tang@ailabs.tw>
2025-01-16 00:07:46 +0800

    Added updator config

--- src-tauri/src/infrastructure/updater.rs
+++ src-tauri/src/infrastructure/updater.rs
@@ -4,7 +4,7 @@ use tauri_plugin_updater::UpdaterExt;
 pub async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
     if let Some(update) = app.updater()?.check().await? {
         let mut downloaded = 0;
-
+        println!("{:#?}", update.download_url);
         update
             .download_and_install(
                 |chunk_length, content_length| {
